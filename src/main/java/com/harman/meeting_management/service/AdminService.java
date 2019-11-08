2cf7fc71fef3dd4eaa4f10c708fd7555b3a8936e
xulm <xlm951024@163.com>
2019-11-08 17:04:16 +0800

    sign in is achieved

---
+++ src/main/java/com/harman/meeting_management/service/AdminService.java
@@ -0,0 +1,8 @@
+package com.harman.meeting_management.service;
+
+/**
+ * @author L.Willian
+ * @date 11/8/2019 2:45 PM
+ */
+public interface AdminService {
+}
---
+++ src/main/java/com/harman/meeting_management/service/Impl/AdminServiceImpl.java
@@ -0,0 +1,8 @@
+package com.harman.meeting_management.service.Impl;
+
+/**
+ * @author L.Willian
+ * @date 11/8/2019 2:45 PM
+ */
+public class AdminServiceImpl {
+}
