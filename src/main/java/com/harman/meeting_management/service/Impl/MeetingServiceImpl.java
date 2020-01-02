744591de3a3cb0237628d420fa11c38e110994d4
Akihi <akihi@AkihideMacBook-Pro.local>
2020-01-02 09:56:03 +0800

    添加会议相关查询

--- src/main/java/com/harman/meeting_management/service/Impl/MeetingServiceImpl.java
+++ src/main/java/com/harman/meeting_management/service/Impl/MeetingServiceImpl.java
@@ -2,10 +2,13 @@ package com.harman.meeting_management.service.Impl;
 
 import com.harman.meeting_management.entity.Meeting;
 import com.harman.meeting_management.mapper.MeetingMapper;
+import com.harman.meeting_management.mapper.UserMapper;
 import com.harman.meeting_management.service.MeetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author L.Willian
@@ -16,11 +19,39 @@ public class MeetingServiceImpl implements MeetingService {
     @Autowired
     private MeetingMapper meetingMapper;
 
+    @Autowired
+    private UserMapper userMapper;
+
     public int addMeeting(Meeting meetingDto) throws DataAccessException {
         return meetingMapper.insertSelective(meetingDto);
     }
 
     public int cancelMeeting(Meeting meetingDto) {
         return meetingMapper.updateByPrimaryKeySelective(meetingDto);
+    }
+
+    @Override
+    public List<Meeting> searchMeeting(String meetingName, String roomName, String reserverName, String reserveFromDate, String reserveToDate, String meetingFromDate, String meetingToDate) {
+        return null;
+    }
+
+    @Override
+    public Meeting getMeetingDetailsByMeetingId(long mid) {
+        return null;
+    }
+
+    @Override
+    public List<Meeting> getCanceledMeeting(int uId) {
+        return null;
+    }
+
+    @Override
+    public List<Meeting> getMeeting7Days(int uId) {
+        return null;
+    }
+
+    @Override
+    public List<Meeting> getMyMeeting(long uId) {
+        return null;
     }
 }
--- src/main/java/com/harman/meeting_management/service/MeetingService.java
+++ src/main/java/com/harman/meeting_management/service/MeetingService.java
@@ -2,6 +2,8 @@ package com.harman.meeting_management.service;
 
 import com.harman.meeting_management.entity.Meeting;
 
+import java.util.List;
+
 /**
  * @author L.Willian
  * @date 11/11/2019 4:05 PM
@@ -23,5 +25,33 @@ public interface MeetingService {
      * @return
      */
     public int cancelMeeting(Meeting meetingDto);
+
+    /**
+     * 查询会议
+     */
+
+    List<Meeting> searchMeeting(String meetingName, String roomName, String reserverName, String reserveFromDate, String reserveToDate, String meetingFromDate, String meetingToDate);
+
+    /**
+     * 会议的详细信息
+     */
+    Meeting getMeetingDetailsByMeetingId(long mid);
+
+    /**
+     * 得到取消的会议
+     */
+    List<Meeting> getCanceledMeeting(int uId);
+
+
+    /**
+     * 得到7天的会议信息
+     */
+    List<Meeting> getMeeting7Days(int uId);
+
+    /**
+     * 得到我的会议信息
+     */
+
+    List<Meeting> getMyMeeting(long uId);
 
 }
